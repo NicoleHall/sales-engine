90e32bdbdb0e90513db206b4d9de38be4783a5e3
Nicole Hall <nicolealexandrahall@gmail.com>
2015-08-06 20:19:46 -0600

    customer_test.rb all passing

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -1,6 +1,11 @@
-require 'test_helper'
+require_relative 'test_helper'
 require_relative "../lib/sales_engine"
+require_relative "../lib/customers.rb"
 
 class CustomersTest < SeTest
+
+  def test_it_has_a_class
+    assert Customer
+  end
 
 end

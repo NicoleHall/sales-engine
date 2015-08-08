a40c7e43399f5d28ab0b40aadbbe728591a3efbc
Nicole Hall <nicolealexandrahall@gmail.com>
2015-08-07 20:04:40 -0600

    I just noticed an error on line 4 of invoices_test.  I have corrected it.

--- test/invoices_test.rb
+++ test/invoices_test.rb
@@ -1,7 +1,7 @@
 require_relative 'test_helper.rb'
 require_relative "../lib/sales_engine"
 
-class TransactionTest < SeTest
+class InvoicesTest < SeTest
 
   def test_it_has_a_class
     assert Invoices
